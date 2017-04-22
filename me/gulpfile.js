d60a79dda5e80eade582a036075f755827c617a3
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-04-21 21:48:43 -0400

    autoprefixer and watcher

--- me/gulpfile.js
+++ me/gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function () {
   console.log('gulp is running');
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 gulp.task('styles', function() {
   gulp.src('./sass/**/*.scss')
       .pipe(sass().on('error', sass.logError)) // logs error and continues with build instead of break
-      .pipe(gulp.dest('./css'))
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./css'));
 });
