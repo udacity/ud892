668cb3e5adcbdc334a8f1f406a5f2e95fe6ea63d
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-04-21 20:43:52 -0400

    gulpfile and sass

--- me/gulpfile.js
+++ me/gulpfile.js
@@ -1,5 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
 
 gulp.task('default', function () {
   console.log('gulp is running');
-})
+});
+
+gulp.task('styles', function() {
+  gulp.src('./sass/**/*.scss')
+      .pipe(sass().on('error', sass.logError)) // logs error and continues with build instead of break
+      .pipe(gulp.dest('./css'))
+});
