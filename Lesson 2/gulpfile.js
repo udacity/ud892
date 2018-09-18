d59d2b423c891772f80863e0dc1cbe7292061bbb
Andrew Wong <hbkwong@ucdavis.edu>
2018-09-18 14:30:06 -0700

    Update Lesson 2 gulpfile

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -1,16 +1,19 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
-gulp.task('default', function() {
-	console.log('hello world!');
+gulp.task("default", function() {
+  console.log("hello world!");
 });
 
-gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('./css'));
+gulp.task("styles", function() {
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./css"));
 });
