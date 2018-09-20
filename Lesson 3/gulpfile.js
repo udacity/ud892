20a59071ac716d5e5873609f30b75ab37f846a31
Andrew Wong <hbkwong@ucdavis.edu>
2018-09-19 23:05:03 -0700

    Update Lesson 3 gulpfile

--- Lesson 3/gulpfile.js
+++ Lesson 3/gulpfile.js
@@ -1,22 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 
-gulp.task('default', ['styles'], function() {
-	gulp.watch('sass/**/*.scss', ['styles']);
+gulp.task("default", ["styles"], function() {
+  gulp.watch("sass/**/*.scss", ["styles"]);
 
-	browserSync.init({
-		server: './'
-	});
+  browserSync.init({
+    server: "./"
+  });
 });
 
-gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
-		.pipe(gulp.dest('./css'))
-		.pipe(browserSync.stream());
+gulp.task("styles", function() {
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 });
